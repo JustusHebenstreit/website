cca104ac2012a2c53bfb73f6018ff1cbca168176
JustusHebenstreit <justus@selinafinance.co.uk>
2022-04-17 21:27:51 +0100

    feat: Added landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,58 +1,34 @@
-import Container from '../components/container'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
-import Intro from '../components/intro'
-import Layout from '../components/layout'
-import { getAllPosts } from '../lib/api'
-import Head from 'next/head'
-import { CMS_NAME } from '../lib/constants'
-import Post from '../types/post'
+import {
+  GitHubLogoIcon,
+  LinkedInLogoIcon,
+  TwitterLogoIcon,
+} from '@radix-ui/react-icons';
 
-type Props = {
-  allPosts: Post[]
-}
-
-const Index = ({ allPosts }: Props) => {
-  const heroPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
+const Index = () => {
   return (
     <>
-      <Layout>
-        <Head>
-          <title>Next.js Blog Example with {CMS_NAME}</title>
-        </Head>
-        <Container>
-          <Intro />
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
-        </Container>
-      </Layout>
+      <div className='bg-slate-100 flex justify-center items-center text-8xl flex-col p-10 lg:text-9xl h-screen'>
+        <div className='flex flex-col w-screen item-start'>
+          <h1>HELLO.</h1>
+        </div>
+        <div className='flex flex-col w-screen items-end'>
+          <h1>I AM</h1>
+          <h1>JUSTUS</h1>
+        </div>
+        <div className='flex space-x-4 mt-10'>
+          <a href='https://github.com/JustusHebenstreit'>
+            <GitHubLogoIcon className='w-[70px] h-[70px]' />
+          </a>
+          <a href='https://twitter.com/ThisIsJustUs'>
+            <TwitterLogoIcon className='w-[70px] h-[70px]' />
+          </a>
+          <a href='https://www.linkedin.com/in/justushebenstreit'>
+            <LinkedInLogoIcon className='w-[70px] h-[70px]' />
+          </a>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Index
-
-export const getStaticProps = async () => {
-  const allPosts = getAllPosts([
-    'title',
-    'date',
-    'slug',
-    'author',
-    'coverImage',
-    'excerpt',
-  ])
-
-  return {
-    props: { allPosts },
-  }
-}
+export default Index;
